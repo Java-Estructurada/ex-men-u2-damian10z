047766e4a8e17524bd4ba03397d6c28c3aae02de
damian10z <Damianzazueta306@gmail.com>
2026-03-11 13:48:51 +0000

    va1

--- cotizadorViajes.java
+++ cotizadorViajes.java
@@ -1,1 +1,8 @@
-
+public class CotizadorViajes {
+    public static double CalcularTotalVuelos (int adultos, int ninos, double tarifaNoche){
+        double costoAdultosNoche = adultos * tarifaNoche;
+        double costoNinosNoche * (tarifaNoche * 0.30);
+        return(costoAdultosNoche + costoNinosNoche) * noches; }
+    
+    
+    }
